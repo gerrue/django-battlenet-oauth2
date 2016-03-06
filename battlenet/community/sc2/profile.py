a97efab0d0efb0721ba11e52e2eeb06801b435f5
gerrue <thomas@proxims.com>
2016-03-05 22:53:07 -0500

    Create profile.py in sc2
    Makes requests for SC2 Profiles by setting an ID and NAME. You must have the ID from SC2 OAUTH PROFILE to use this

---
+++ battlenet/community/sc2/profile.py
@@ -0,0 +1,24 @@
+from battlenet.community import Community
+
+class Profile(Community):
+
+    ENDPOINT = '/sc2/profile/%d/1/%s/'
+
+    def __init__(self, *args, **kwargs):
+        super(Profile, self).__init__(*args, **kwargs)
+        self.id = kwargs.get('id', None)
+        self.name = kwargs.get('name', None)
+
+    def get(self, id=None, name=None):
+        if name:
+            self.name = name
+        else:
+            if not self.name:
+                raise ValueError('Name required.')
+        if id:
+            self.id = id
+        else:
+            if not self.id:
+                raise ValueError('ID is required, Need SC2 OAUTH PROFILE ID for this')
+
+        return self.make_request(self.ENDPOINT % (self.id, self.name))
